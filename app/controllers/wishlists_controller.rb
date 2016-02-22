7584bcc446e49e89b21ffba2b5f02d30b3efbf51
Tori <victoria.ashley.wood@gmail.com>
2016-02-22 16:51:30 -0500

    updated the routes file with all wishlist routes and added the new and edit actions and views

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,7 +1,35 @@
 class WishlistsController < ApplicationController
   def index
+  	@wishlists = Wishlist.all
   end
 
   def show
   end
+
+  def create
+  	@wishlist = Wishlist.create(wishlist_params)
+
+  	if @user.save
+  		flash[:success] = "Wishlist created successfully!"
+  		redirect_to wishlists_path
+  	else
+  		flash[:error] = "There was a problem creating this wishlist. Try again."
+  		redirect_to new_wishlist_path
+  	end
+  end
+
+
+private
+
+	def wishlist
+		@wishlist = Wishlist.find(params[:id])		
+	end
+
+	helper_method :wishlist
+	
+	def wishlist_params
+		params.require(:wishlist).permit(:id, :name)
+	end
+
+
 end
