7e1a60648c7345c67ae8509f9bacf61d9c39f3d5
Tori <victoria.ashley.wood@gmail.com>
2016-02-22 17:21:07 -0500

    added action links on the wishlist show page

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -7,6 +7,10 @@ class WishlistsController < ApplicationController
   	@wishlist = Wishlist.new
   end
 
+  def edit
+    @wishlist = wishlist
+  end
+
   def show
   end
 
@@ -22,11 +26,28 @@ class WishlistsController < ApplicationController
   	end
   end
 
+  def update
+    @wishlist = Wishlist.update(params[:id], wishlist_params)
+
+    if @wishlist.save
+      flash[:success] = "Wishlist updated successfully!"
+      redirect_to wishlist_path
+    else
+      flash[:error] = "There was a problme updating this wishlist. Try again."
+      redirect_to edit_wishlist_path
+    end
+    
+  end
+
+  def destroy
+    wishlist.destroy
+    redirect_to wishlists_path
+  end
 
 private
 
 	def wishlist
-		@wishlist = Wishlist.find(params[:id])		
+		wishlist = Wishlist.find(params[:id])		
 	end
 
 	helper_method :wishlist
