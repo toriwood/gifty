99f989595fbc4cdb84927acd53c69b75f6d8e6c5
Tori <victoria.ashley.wood@gmail.com>
2016-02-23 18:47:19 -0500

    added birthday column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223233412) do
+ActiveRecord::Schema.define(version: 20160223234649) do
 
   create_table "gifts", force: :cascade do |t|
     t.string   "name"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20160223233412) do
     t.integer  "age"
     t.string   "gender"
     t.text     "interests"
+    t.datetime "birthday"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
