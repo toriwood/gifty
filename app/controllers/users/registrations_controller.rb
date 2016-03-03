bab734914606fd9340c85969b7b166c67f2b6ab8
Tori <victoria.ashley.wood@gmail.com>
2016-03-02 20:14:10 -0500

    updated the devise registrations controller and views to allow users to select and save interests

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -13,12 +13,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit(:age, :gender, :name, :username, :interests, :birthday,
-        :email, :password, :password_confirmation)
+      u.permit(:age, :gender, :name, :username, :birthday,
+        :email, :password, :password_confirmation, {:interests => []})
     end
     devise_parameter_sanitizer.for(:account_update) do |u|
-      u.permit(:age, :gender, :name, :username, :interests, :birthday,
-        :email, :password, :password_confirmation, :current_password)
+      u.permit(:age, :gender, :name, :username, :birthday,
+        :email, :password, :password_confirmation, :current_password, {:interests => []})
     end
   end
 end
