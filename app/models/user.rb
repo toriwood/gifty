d1a489ee7a6b73d7fff1ccb193b1e674fcd0a850
Tori <victoria.ashley.wood@gmail.com>
2016-03-06 13:24:33 -0500

    updated user to have attached image

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,8 @@ class User < ActiveRecord::Base
   	(Date.today - self.birthday).to_i / 365
   end
 
+
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301162012) do
+ActiveRecord::Schema.define(version: 20160306143941) do
 
   create_table "gifts", force: :cascade do |t|
     t.string   "name"
@@ -54,6 +54,10 @@ ActiveRecord::Schema.define(version: 20160301162012) do
     t.string   "gender"
     t.text     "interests"
     t.date     "birthday"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
