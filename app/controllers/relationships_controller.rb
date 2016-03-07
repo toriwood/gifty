44087d4f51eca3c285a44f2e64af2da628a61984
Tori <victoria.ashley.wood@gmail.com>
2016-03-07 15:46:33 -0500

    updated users with more views and a partial to show wishlists, gifts, followers, and following

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,7 +2,7 @@ class RelationshipsController < ApplicationController
   def create
   	@relationship = current_user.relationships.build(friend_id: params[:friend_id])
   	
-  	!current_user.following.include?(params[:friend_id]) ? current_user.following << params[:friend_id] : nil
+  	!current_user.following.include?(params[:friend_id]) ? current_user.following << params[:friend_id] : current_user.following
   	current_user.save
 
 		if @relationship.save
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   get 'relationships/create'
   get 'relationships/destroy'
 
+  get 'users/:id/gifts' => 'users#gifts', as: "user_gifts"
+  get 'users/:id/followers' => 'users#followers', as: "user_followers"
+  get 'user/:id/following' => 'users#following', as: "user_following"
+
+
   get '/interests' => 'interests#index'
   get '/interests/:id' => 'interests#show', as: "interest"
   match '/gifts/update_image/:id' => 'gifts#update_image', via: [:get, :post]
