8b71d877106dccaeaf78bd5b6d3229d359f8830f
Tori <victoria.ashley.wood@gmail.com>
2016-03-10 17:29:11 -0500

    updated default user image and styled notifications

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,8 @@ class User < ActiveRecord::Base
 
 
   has_attached_file :image, 
-    styles: { medium: "300x300#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
+    styles: { medium: "300x300#", thumb: "100x100#" }, 
+      default_url: ActionController::Base.helpers.asset_path("missing.png")
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
