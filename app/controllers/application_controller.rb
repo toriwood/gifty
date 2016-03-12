573709ca56df646a75794ab2ee5a501293e55f1e
Tori <victoria.ashley.wood@gmail.com>
2016-03-12 17:40:53 -0500

    refactored notifications to show in feed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,13 @@ class ApplicationController < ActionController::Base
 	end
 
 	def notifications
-		@notifications = []
+		events = []
+		notifications = []
 		users = current_user.celebrating
 
 		users.each do |user|
 			User.find(user.to_i).special_days.each do |holiday, day|
-			day_array = []
+			days = {}
 				if Date.new(Date.today.year, day.mon, day.mday) > Date.today
 					date = Date.new(Date.today.year, day.mon, day.mday)
 				else
@@ -22,15 +23,33 @@ class ApplicationController < ActionController::Base
 				if date.between?(Date.today, Date.today + 2.week)
 					number_of_days = (date - Date.today).to_i
 					day = date.strftime("%a, %b #{date.day.ordinalize}")
-				day_array << number_of_days
-				day_array << {holiday => day}	
-				@notifications << [user, day_array]
+
+				days[:user] = user
+				days[:holiday] = holiday
+				days[:date] = day
+				days[:number] = number_of_days
+
+				events << days
 				end
 			end
 		end
 
-		@notifications.sort_by { |n| n[1][1].values.first }
-			
+		events = events.group_by { |e| e[:holiday] }
+		events.each do |k, v|
+			usernames = []
+			v.each do |vi|
+				usernames << User.find(vi[:user]).username.titlecase
+			end
+
+			if k.downcase == "birthday"
+			notifications << "#{usernames.to_sentence} will be celebrating soon! They'll be celebrating their #{k.downcase} in #{v.first[:number]} days on #{v.first[:date]}."
+			else	
+			notifications << "#{usernames.to_sentence} will be celebrating soon! They'll be celebrating #{k} in #{v.first[:number]} days on #{v.first[:date]}."
+			end
+		end
+
+		notifications
+
 	end
 
   # Prevent CSRF attacks by raising an exception.
