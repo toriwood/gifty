ceb4ca8603541b1bf2b53d094b251285046e572b
Tori <victoria.ashley.wood@gmail.com>
2016-03-15 11:15:08 -0400

    fixed image file path

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,7 @@ class User < ActiveRecord::Base
 
   has_attached_file :image, 
     styles: { medium: "300x300#", thumb: "100x100#" }, 
-      default_url: 'images/missing.png'
+      default_url: 'missing.png'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
