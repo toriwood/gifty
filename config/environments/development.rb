e9e190a6b760e55532589a1db67fafb0e9c5379f
Tori <victoria.ashley.wood@gmail.com>
2016-11-29 21:42:51 -0500

    updated action mailer config for production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,6 +25,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send.
+  config.action_mailer.default_url_options = { :host => 'localhost' }
   config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
